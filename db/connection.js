9b24b9476ab248746dc39350f08a49f3873bdfe1
Racquel Munoz <racquelg85@hotmail.com>
2022-09-28 13:55:37 -0500

    modularize code

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'Spring@1',
+  database: 'election'
+});
+
+
+
+module.exports = db;
